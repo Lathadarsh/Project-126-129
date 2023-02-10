bcd2a04e0dcdd8979278a22bdb9ae5a30d118af9
Lathadarsh <97331941+Lathadarsh@users.noreply.github.com>
2023-02-10 17:59:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-song1="";
-song2="";
+song1 = "";
+song2 = "";
 lwX = 0;
 lwY = 0;
 rwX = 0;
 rwY = 0;
+scoreLW = 0;
+scoreRW = 0;
+song1_status = "";
+song2_status = "";
 function preload() {
     song1 = loadSound("Harry potter theme song.mp3");
     song2 = loadSound("Peter Pan song.mp3");
 }
 
 function setup() {
-canvas = createCanvas(600,500);
-canvas.center();
+    canvas = createCanvas(600, 500);
+    canvas.center();
 
-video = createCapture(VIDEO);
-video.hide();
+    video = createCapture(VIDEO);
+    video.hide();
 
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
@@ -25,7 +29,30 @@ function modelLoaded() {
 }
 
 function draw() {
-image(video,0,0,600,500);
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("red");
+    stroke("red");
+
+    if (scoreRW > 0.2) {
+        circle(rwX, rwY, 20);
+        song2.stop();
+        if(song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing Harry Potter Theme Song";
+        }
+    }
+    if (scoreLW > 0.2) {
+        circle(lwX, lwY, 20);
+        song1.stop();
+        if(song2_status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing Peter Pan Song";
+        }
+    }
 }
 function play() {
     song1.play();
@@ -34,7 +61,7 @@ function play() {
 }
 
 function gotPoses(results) {
-    if(results.length > 0) {
+    if (results.length > 0) {
         console.log(results);
         lwX = results[0].pose.leftWrist.x;
         lwY = results[0].pose.leftWrist.y;
@@ -43,5 +70,9 @@ function gotPoses(results) {
         rwX = results[0].pose.rightWrist.x;
         rwY = results[0].pose.rightWrist.y;
         console.log("Right Wrist X = " + rwX + " Right Wrist Y = " + rwY);
+
+        scoreLW = results[0].pose.keypoints[9].score;
+        scoreRW = results[0].pose.keypoints[10].score;
+        console.log("ScoreLEftWrist = " + scoreLW + " ScoreRightWrist = " + scoreRW);
     }
 }
